cbd3d40b365171f575905c8978be00b1b8bad8e9
Vincenius <vincent.will@vaitrade.de>
2021-05-27 17:11:19 +0200

    add sizes to image

--- components/Image.js
+++ components/Image.js
@@ -23,13 +23,19 @@ const Image = ({ data, windowWidth, columns, openImage }) => {
       offset={100}
     >
       <img
-        sizes="(max-width: 1400px) 100vw, 1400px"
+        sizes={`
+          (min-width: 1200px) 1600px,
+          (min-width: 900px) 900px,
+          (min-width: 500px) 600px,
+          (min-width: 320px) 320px,
+          100vw
+        `}
         srcSet={`
           ${imagesPath}w_320_${data.fileName} 320w,
           ${imagesPath}w_600_${data.fileName} 600w,
           ${imagesPath}w_900_${data.fileName} 900w,
           ${imagesPath}w_1200_${data.fileName} 1200w,
-          ${imagesPath}${data.fileName} 1400w
+          ${imagesPath}${data.fileName} 1600w
         `}
         src={`${imagesPath}${data.fileName}`}
         alt={data.description || data.fileName}
